c9130a648059d97d7dae43ae08b2e5bc863b5c99
Aeyzechiah Vasquez <az_vasquez@ultimatesoftware.com>
2019-05-21 13:41:30 -0400

    Decoding module

---
+++ MorseExpress/MorseExpress MessagesExtension/DecodeMorse.swift
@@ -0,0 +1,51 @@
+var standardMorseDict = [
+    ".-":       "a",
+    "-...":     "b",
+    "-.-.":     "c",
+    "-..":      "d",
+    ".":        "e",
+    "..-.":     "f",
+    "--.":      "g",
+    "....":     "h",
+    "..":       "i",
+    ".---":     "j",
+    "-.-":      "k",
+    ".-..":     "l",
+    "--":       "m",
+    "-.":       "n",
+    "---":      "o",
+    ".--.":     "p",
+    "--.-":     "q",
+    ".-.":      "r",
+    "...":      "s",
+    "-":        "t",
+    "..-":      "u",
+    "...-":     "v",
+    ".--":      "w",
+    "-..-":     "x",
+    "-.--":     "y",
+    "--..":     "z",
+    "-----":    "0",
+    ".----":    "1",
+    "..---":    "2",
+    "...--":    "3",
+    "....-":    "4",
+    ".....":    "5",
+    "-....":    "6",
+    "--...":    "7",
+    "---..":    "8",
+    "----.":    "9",
+]
+
+func decodeCharacter(morseCode: String) -> String {
+    return standardMorseDict[morseCode]!
+}
+
+func decodeString(morseCode: String) -> String {
+    return morseCode
+        .split(separator: " ")
+        .map{ decodeCharacter(morseCode: String($0)) }
+        .joined(separator: "")
+}
+
+print(decodeString(morseCode: ". .- -"))
