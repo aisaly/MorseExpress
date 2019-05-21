0657f277d95dc5cf3e76c24a70636039fa7fdeca
Alex Isaly <alexisaly97@gmail.com>
2019-05-21 15:58:21 -0400

    added preview view

---
+++ MorseExpress/MorseExpress MessagesExtension/FullViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FullViewController.swift
+//  MorseExpress MessagesExtension
+//
+//  Created by Alexandra Isaly on 5/21/19.
+//  Copyright © 2019 Alexandra Isaly. All rights reserved.
+//
+
+import UIKit
+
+class FullViewController: UIViewController {
+
+    @IBOutlet weak var dotButton: UIButton!
+    @IBOutlet weak var dashButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dotButton.layer.cornerRadius = 6
+        dashButton.layer.cornerRadius = 6
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func dotHandle(_ sender: Any) {
+    }
+    
+    @IBAction func dashHandle(_ sender: Any) {
+    }
+}
