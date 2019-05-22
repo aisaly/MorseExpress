76c7ad77813b988ba32f6e1acb66ef77adb1e785
MadisonF <madison_fileman@ultimatesoftware.com>
2019-05-22 10:54:11 -0400

    button merge with logic, cleaner version coming

--- MorseExpress/MorseExpress MessagesExtension/PreviewViewController.swift
+++ MorseExpress/MorseExpress MessagesExtension/PreviewViewController.swift
@@ -7,9 +7,60 @@
 //
 
 import UIKit
+import Messages
+import Foundation
 
-class PreviewViewController: UIViewController {
+class PreviewViewController: MSMessagesAppViewController {
 
+    var standardMorseDict = [
+        ".-":       "a",
+        "-...":     "b",
+        "-.-.":     "c",
+        "-..":      "d",
+        ".":        "e",
+        "..-.":     "f",
+        "--.":      "g",
+        "....":     "h",
+        "..":       "i",
+        ".---":     "j",
+        "-.-":      "k",
+        ".-..":     "l",
+        "--":       "m",
+        "-.":       "n",
+        "---":      "o",
+        ".--.":     "p",
+        "--.-":     "q",
+        ".-.":      "r",
+        "...":      "s",
+        "-":        "t",
+        "..-":      "u",
+        "...-":     "v",
+        ".--":      "w",
+        "-..-":     "x",
+        "-.--":     "y",
+        "--..":     "z",
+        "-----":    "0",
+        ".----":    "1",
+        "..---":    "2",
+        "...--":    "3",
+        "....-":    "4",
+        ".....":    "5",
+        "-....":    "6",
+        "--...":    "7",
+        "---..":    "8",
+        "----.":    "9",
+    ]
+    
+    func decodeCharacter(morseCode: String) -> String {
+        return standardMorseDict[morseCode]!
+    }
+   
+    
+    var currentMessage: MSMessage!
+    var currentStringDecoded = ""
+    var currentString = ""
+    var active = false
+    
     @IBOutlet weak var dotButton: UIButton!
     @IBOutlet weak var dashButton: UIButton!
     
@@ -29,10 +80,77 @@ class PreviewViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    private var idleTimer: Timer?
+    private var timeoutInSeconds = 1.0
+    
+    private func resetIdleTimer() {
+        if let idleTimer = idleTimer {
+            idleTimer.invalidate()
+        }
+        
+        idleTimer = Timer.scheduledTimer(
+            timeInterval: timeoutInSeconds,
+            target: self,
+            selector: #selector(PreviewViewController.timeHasExceeded),
+            userInfo: nil,
+            repeats: false
+        )
+    }
+    
+    private func onPause(){
+        currentString += " "
+        decode()
+        updateBubbleMessage()
+    }
+    
+    private func insertText(text: String) {
+        currentString += text
+        print(currentString)
+        resetIdleTimer()
+        updateBubbleMessage()
+    }
+    
+    private func decode() {
+        let pattern = "\\w*([\\.\\-]+)\\ "
+        let regex = try! NSRegularExpression(pattern: pattern, options: [])
+        let matches = regex.matches(in:currentString, range:NSMakeRange(0, currentString.utf16.count))
+        if (matches == []) {
+            return
+        }
+        var decodedString = ""
+        matches.forEach { match in
+            let range = match.range(at:1)
+            let swiftRange = Range(range, in: currentString)
+            let morseChar = currentString[swiftRange!]
+            let textChar = decodeCharacter(morseCode: String(morseChar))
+            decodedString += textChar
+        }
+        currentStringDecoded = decodedString
+        print(currentString + " -> " + currentStringDecoded)
+    }
+    
+    private func updateBubbleMessage(){
+        let layout = MSMessageTemplateLayout()
+        layout.caption = currentStringDecoded
+        let message = MSMessage()
+        message.layout = layout
+        activeConversation?.insert(message, completionHandler: nil)
+    }
+    
+    
+    
+    @objc private func timeHasExceeded() {
+        onPause()
+    }
+    
+    
 
     @IBAction func dotHandle(_ sender: Any) {
+        insertText(text: ".")
     }
     @IBAction func dashHandle(_ sender: Any) {
+        insertText(text: "-")
     }
     
 }
