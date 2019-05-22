201e1ab6bfe792e1fa0c7ced58785f18348c4e5c
Aeyzechiah Vasquez <az_vasquez@ultimatesoftware.com>
2019-05-22 18:12:08 -0400

    text preview

--- MorseExpress/MorseExpress MessagesExtension/PreviewViewController.swift
+++ MorseExpress/MorseExpress MessagesExtension/PreviewViewController.swift
@@ -39,12 +39,9 @@ class PreviewViewController: MSMessagesAppViewController {
     @IBOutlet weak var shiftButton: UIButton!
     @IBOutlet weak var CompleteButton: UIButton!
     
-    var isMorse = true
-    
-    
     @IBAction func shiftHandle(_ sender: Any) {
-        isMorse = !isMorse
-        if (isMorse) {
+        MessagesViewController.toggleEmoji()
+        if (MessagesViewController.isMorse) {
             print("morse time!")
             shiftButton.isSelected = false
         }
@@ -57,7 +54,6 @@ class PreviewViewController: MSMessagesAppViewController {
         print("send it")
     }
     
-    private var nc: NotificationCenter = NotificationCenter.default
     
     
     @IBOutlet weak var dotButton: UIButton!
@@ -65,100 +61,15 @@ class PreviewViewController: MSMessagesAppViewController {
     
     @IBAction func dotHandle(_ sender: Any) {
         MessagesViewController.sendDot()
+        msgPreview.text = MessagesViewController.getText()
+
     }
     
     @IBAction func dashHandle(_ sender: Any) {
         MessagesViewController.sendDash()
+        msgPreview.text = MessagesViewController.getText()
     }
     
     
     @IBOutlet weak var msgPreview: UILabel!
-    
-    
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~ timer handling ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    private var idleTimer: Timer?
-    private var timeoutInSeconds: Double = 1.0
-    
-    private func resetIdleTimer() {
-        if let idleTimer = idleTimer {
-            idleTimer.invalidate()
-        }
-        
-        idleTimer = Timer.scheduledTimer(
-            timeInterval: timeoutInSeconds,
-            target: self,
-            selector: #selector(PreviewViewController.timeHasExceeded),
-            userInfo: nil,
-            repeats: false
-        )
-    }
-    
-    @objc private func timeHasExceeded() {
-        onPause() // when timeout has happened, reflect that in the text
-        //activeConversation?.insert(self.getMessage())
-    }
-    
-    
-    
-    
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~ textual input handling ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    private var currentMessage: MSMessage = MSMessage()
-    var currentStringDecoded = "" //current readable string (morse2this)
-    var currentString = "" //current morse string
-    var toBeDecoded = ""
-    var active = false
-    
-    
-    
-    private func insertText(text: String) {
-        currentString += text
-        toBeDecoded += text
-    }
-    
-    private func updateBubbleMessage() //called everytime youre adding new characters
-    {
-        //print("tobedecoded " + toBeDecoded)
-        currentStringDecoded += decode(morseCode: toBeDecoded)
-        print(currentStringDecoded)
-        msgPreview.text = currentStringDecoded
-    }
-    
-    private func decode(morseCode: String) -> String {
-        let pattern = "\\w*([\\.\\-]+)\\ "
-        let regex = try! NSRegularExpression(pattern: pattern, options: [])
-        let matches = regex.matches(in:currentString, range:NSMakeRange(0, currentString.utf16.count))
-        if (matches == []) {
-            return ""
-        }
-        var char = ""
-        //print(toBeDecoded + " to be decoded")
-        matches.forEach { match in
-            let range = match.range(at:1)
-            let swiftRange = Range(range, in: currentString)
-            let morseChar = currentString[swiftRange!]
-            char = isMorse ?
-                 decodeCharacter(morseCode:String(morseChar) ) //if u want morse to text
-            :
-                morse2emoji(code: String(morseChar)) //if u want morse to emojis
-            //print(char + "char")
-        }
-        return char
-    }
-    
-    
-    private func onPause(){ //DOESNT ADD SPACE TO THE DECODED STRING
-        currentString += " "
-        
-        updateBubbleMessage()
-        toBeDecoded = ""
-    }
-    
-    
-    func cleanup(){
-        currentString = ""
-        currentStringDecoded = ""
-        isMorse = true
-        msgPreview.text = "..."
-    }
-
 }
